58cca0ea41d1026c59612ce08c33460cdc4b7d21
leo-hardy <40430728+leo-hardy@users.noreply.github.com>
2020-12-06 17:33:02 +0100

    Update data_processing.py

--- data_processing.py
+++ data_processing.py
@@ -1,10 +1,13 @@
 import pandas as pd
 import numpy as np
+import matplotlib as pt
 
 radar = pd.read_csv('./Radar_Traffic_Counts.csv')
-radar.head()
-radar.describe()
+radar['location_name']=radar.location_name.apply(lambda x: x.strip()) 
 
+byhour = pd.DataFrame( radar.groupby(['location_name', 'Year', 'Month', 'Day', 'Time Bin' ])['Volume'].sum() )
+byhour = byhour.sort_index(level=4).sort_index(level=3).sort_index(level=2).sort_index(level=1)
 
-byhour = pd.DataFrame( radar.groupby(['Year', 'Month', 'Day', 'Hour', 'Minute', 'location_latitude', 'location_longitude'])['Volume'].sum() )
-byhour.groupby( level=['Year', 'Month', 'Day', 'Hour', 'Minute'] ).count().describe()
+my_radar = 'LAMAR BLVD / SANDRA MURAIDA WAY (Lamar Bridge)'
+my_radar = pd.DataFrame( detailed.loc[ my_radar ] )
+my_radar = my_radar.interpolate()
