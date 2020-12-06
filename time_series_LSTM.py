2c93f1f895836ff6f42bc3728cfbbae1d9e7db4e
Paul Lecomte <paullecomte07@gmail.com>
2020-12-06 15:09:55 +0100

    creation de la classe et du forward

--- time_series_LSTM.py
+++ time_series_LSTM.py
@@ -1,2 +1,13 @@
 import torch
 import torch.nn as nn
+
+
+class LSTM(nn.Module):
+    # On a qu'une seule variable d'entrée qui est le nombre de véhicules détectés sur un radar
+    
+    def __init__(self, input_size=1, hidden_layer_size=100, output_size=1):
+
+        super().__init__()
+
+    def forward():
+        return 1
