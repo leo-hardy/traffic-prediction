dd8555ef3b207da9d98e1eecc62faef66c8c74cf
Paul Lecomte <paullecomte07@gmail.com>
2020-12-12 18:39:22 +0100

    add a function de shuffle the training set

--- time_series_LSTM.py
+++ time_series_LSTM.py
@@ -6,7 +6,7 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 import numpy as np
 import matplotlib.pyplot as plt
-
+import random
 import time
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -28,16 +28,16 @@ Tip 3: Tune batch size and learning rate after tuning all other hyperparameters.
 Implementation de la sliding window
 
 """
-# lecture des données
+# Lecture des données
 start_time = time.time()
 
 data = pd.read_csv("../LAMAR BLVD.csv")
 
 
-# normalisation des donnees entre -1 et 1 en utilisant la fonction MinMaxScaler de la librairie sklearn
+# Normalisation des donnees entre -1 et 1 en utilisant la fonction MinMaxScaler de la librairie sklearn
 scaler = MinMaxScaler( feature_range=(-1, 1) )
 train_data_normalized = scaler.fit_transform( data["LAMAR BLVD / SANDRA MURAIDA WAY (Lamar Bridge)"].to_numpy().reshape(-1, 1) )
-train_data_normalized = train_data_normalized.reshape(1,-1)[0][:5000]
+train_data_normalized = train_data_normalized.reshape(1,-1)[0][50000 :55000]
 
 # length of the window for training, it is the number of previous quarter-hours from which the net learns
 window_length = 4*2
@@ -45,7 +45,7 @@ batch_size = 1
 print( 'hyperparameters : window_length = %s hours' % (window_length/4) )
 
 
-# cette fonction permet de creer une fenetre de valeur avec le label associé.
+# Cette fonction permet de creer une fenetre de valeur avec le label associé.
 def createur_vecteur(sequence, pas):
     seq=[]
     for i in range(0,len(sequence)-pas-1):
@@ -54,7 +54,11 @@ def createur_vecteur(sequence, pas):
 
 
 radar_sequences = createur_vecteur( train_data_normalized, window_length )
+# Obtention du training set et du test set a partir d'une fonction de sklearn
 train_seq, test_seq = train_test_split( radar_sequences, test_size=300 )
+
+# Shuffling the train set
+train_seq = random.shuffle(train_seq)
 
 data_time = time.time()
 print('training and testing set preparation took %s seconds' % (data_time-start_time) )
@@ -75,8 +79,8 @@ class LSTM(nn.Module):
         self.hidden_layer_size = hidden_layer_size
         # définition du module lstm à un seul bloc
         self.lstm = nn.LSTM( input_size=input_size, num_layers=1, hidden_size=self.hidden_layer_size )
-        # définition de le 3 couches denses en sorties du module LSTM
 
+        # définition de la couche dense en sortie du module LSTM
         self.fc1 = nn.Linear( in_features=self.hidden_layer_size, out_features=1)
 
         self.hidden_cell = (
@@ -95,6 +99,7 @@ class LSTM(nn.Module):
         x = self.fc1( x )
         return x
 
+    # Nous remettons à zéro les couches cachées à chaque itération lors de l'apprentissage (Stateless Lstm)
     def reset_hidden_state(self):
         self.hidden_cell = (
             torch.zeros(1, 1, self.hidden_layer_size),
@@ -109,7 +114,7 @@ criterion = nn.MSELoss()
 optimizer = torch.optim.Adam( net.parameters() )
 
 model_time = time.time()
-print('model creation took %s seconds' % (model_time-data_time) )
+print('Model creation took %s seconds' % (model_time-data_time) )
 
 
 """
@@ -141,7 +146,7 @@ for epoch in range( num_epochs ):
 
         loss = criterion( traffic_predicted[0][0], traffic_real )
 
-        #Propagating the error backward
+        # Propagating the error backward
         loss.backward()
 
         # Optimizing the parameters
@@ -150,7 +155,7 @@ for epoch in range( num_epochs ):
 
         count += 1
 
-        # Testing the model
+        # Testing the model every 100 iterations
         if not ( count % 100 ):
             t1 = time.time()
             err = 0
