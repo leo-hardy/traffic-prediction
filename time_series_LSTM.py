5f3f1a0786f1965ab9fbb2fd1b2141a31adfe4b2
leo-hardy <40430728+leo-hardy@users.noreply.github.com>
2020-12-14 20:09:12 +0100

    clean code

--- time_series_LSTM.py
+++ time_series_LSTM.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 import pandas as pd
 import torch.nn.functional as F
@@ -5,43 +6,29 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 import numpy as np
 import matplotlib.pyplot as plt
-#from sklearn.utils import shuffle
 import time
 from model import LSTM
 from createur_vecteur import createur_vecteur
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+start_time = time.time()
+
 ### STYLE DEFINITION ###
 plt.style.use('ggplot')
 
-"""
-HYPERPARAMETERS:
+### MISE EN PLACE DES DONNEES ###
 
-taille de la sliding window
-batch_size (minibatch ?)
-num_epochs
-Tip 3: Tune batch size and learning rate after tuning all other hyperparameters.
-"""
-
-
-
-"""
-
-Implementation de la sliding window
-
-"""
 # Lecture des données
-start_time = time.time()
-
+my_radar = 'LAMAR BLVD / SANDRA MURAIDA WAY (Lamar Bridge)'
 data = pd.read_csv("../LAMAR BLVD.csv")
 
-data_lower_bound = 50000
-data_upper_bound = 55000
+data_lower_bound = 40000
+data_upper_bound = 50000
 
 # Normalisation des donnees entre -1 et 1 en utilisant la fonction MinMaxScaler de la librairie sklearn
 scaler = MinMaxScaler( feature_range=(-1, 1) )
-data_normalized = scaler.fit_transform( data["LAMAR BLVD / SANDRA MURAIDA WAY (Lamar Bridge)"].to_numpy().reshape(-1, 1) )
+data_normalized = scaler.fit_transform( data[ my_radar ].to_numpy().reshape(-1, 1) )
 data_normalized_for_processing = data_normalized.reshape(1,-1)[0][data_lower_bound :data_upper_bound]
 
 # length of the window for training, it is the number of previous quarter-hours from which the net learns
@@ -49,24 +36,24 @@ window_length = 4*24 # for one day
 batch_size = 1
 print( 'hyperparameters : window_length = %s hours' % (window_length/4) )
 
-
-
 radar_sequences = createur_vecteur( data_normalized_for_processing, window_length )
 # Obtention du training set et du test set a partir d'une fonction de sklearn
-train_seq, test_seq = train_test_split( radar_sequences, test_size=50)
-
-
+train_seq, test_seq = train_test_split( radar_sequences, test_size = 1000)
 
 data_time = time.time()
 print('training and testing set preparation took %s seconds' % (data_time-start_time) )
 
 
+
+
+### MODELE ###
+
+# import du modèle
 net = LSTM()
 
 
 # Nous choisissons d'utiliser l'erreur quadratique moyenne comme criterion et l'optimize Adam (choix relativement arbitraire..)
 criterion = nn.MSELoss()
-
 
 learning_rate = 0.001
 
@@ -76,14 +63,10 @@ model_time = time.time()
 print('Model creation took %s seconds' % (model_time-data_time) )
 
 
-"""
 
-Mise en place de la boucle d'apprentissage
+### BOUCLE D'APPRENTISSAGE ###
 
-"""
-
-num_epochs = 1
-
+num_epochs = 4
 
 # Lists for visualization of loss and accuracy
 loss_list = []
@@ -99,7 +82,7 @@ for epoch in range( num_epochs ):
         # Transfering images and labels to GPU if available
         traffic_previous, traffic_real = traffic_previous.to(device), traffic_real.to(device)
 
-        # Initializing a gradient as 0 so there is no mixing of gradient among the batches
+        # Initializing a gradient as 0 
         optimizer.zero_grad()
         net.reset_hidden_state()
 
@@ -115,7 +98,7 @@ for epoch in range( num_epochs ):
 
         count += 1
 
-        # Testing the model every 100 iterations
+        # Testing the model every *checkpoint* iterations
         checkpoint = 200
         if not ( count % checkpoint ):
             t1 = time.time()
@@ -142,7 +125,7 @@ for epoch in range( num_epochs ):
             loss_list.append( np.true_divide( loss_denormalized_sum.detach().numpy() , checkpoint ) )
             loss_denormalized_sum = 0
 
-
+### PRINT RESULTS ###
 
 print('average test overall all test set took %s seconds' % ( sum(duration_test_list)/len(duration_test_list) ) )
 plt.plot( iteration_list, loss_list, color='#32378D', label='Train loss' )
@@ -150,7 +133,7 @@ plt.plot( iteration_list, errors_test_set_list, color='#FF761A', label='Test los
 plt.legend(loc="upper right")
 plt.xlabel( "No. of Iteration" )
 plt.ylabel( "errors" )
-plt.title( "Iterations vs errors_test/train_set, batch size=%s, %s epochs, window of %s 1/4 hours, lr=%s" % ( batch_size, num_epochs, window_length, learning_rate ))
+plt.title( "Iterations vs errors_test/train_set, %s epochs, window of %s 1/4 hours, lr=%s" % ( num_epochs, window_length, learning_rate ))
 plt.show()
 
 
