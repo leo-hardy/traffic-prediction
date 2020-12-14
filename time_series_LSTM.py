5bbf0dff6c616ccc020ad74af4f57edf00736076
Paul Lecomte <paullecomte07@gmail.com>
2020-12-14 19:11:51 +0100

    finishing graphs

--- time_series_LSTM.py
+++ time_series_LSTM.py
@@ -86,6 +86,7 @@ print('Model creation took %s seconds' % (model_time-data_time) )
 Mise en place de la boucle d'apprentissage
 
 """
+
 num_epochs = 1
 
 
@@ -153,13 +154,13 @@ plt.plot( iteration_list, loss_list, color='r', label='Train loss' )
 plt.plot( iteration_list, errors_test_set_list, color='b', label='Test loss'  )
 plt.legend(loc="upper right")
 plt.xlabel( "No. of Iteration" )
-plt.ylabel( "errors_test_set" )
-plt.title( "Iterations vs errors_test_set, batch size=%s, %s epochs, window of %s 1/4 hours, lr=%s" % ( batch_size, num_epochs, window_length, learning_rate ))
+plt.ylabel( "errors" )
+plt.title( "Iterations vs errors_test/train_set, batch size=%s, %s epochs, window of %s 1/4 hours, lr=%s" % ( batch_size, num_epochs, window_length, learning_rate ))
 plt.show()
 
 
 # Plot the forcasting of car flow after the chosen data:
-nb_days_predicted= 4
+nb_days_predicted = 10
 data_realized = data["LAMAR BLVD / SANDRA MURAIDA WAY (Lamar Bridge)"][data_upper_bound : data_upper_bound+4*24*nb_days_predicted].to_numpy()
 data_normalized_to_be_predicted = data_normalized.reshape(1,-1)[0][data_upper_bound : data_upper_bound+4*24*nb_days_predicted]
 
@@ -173,9 +174,9 @@ for traffic_previous, traffic_real in radar_sequences_to_be_predicted:
     traffic_previous, traffic_real = traffic_previous.to(device), traffic_real.to(device)
     traffic_predi = net( traffic_previous ).detach()[0]
     if counter < len(radar_sequences_to_be_predicted[0][0]):
-        current_window = torch.cat((current_window[1:],traffic_predi),0)
+        current_window = torch.cat((current_window[1:],traffic_predi), 0)
     else:
-        current_window = torch.cat((current_window,traffic_predi),0)
+        current_window = torch.cat((current_window,traffic_predi), 0)
 
     # we want int values for sales but we got [0, 1] values in nn
     traffic_predicted = scaler.inverse_transform( net( current_window ).detach().numpy() )
@@ -184,8 +185,11 @@ for traffic_previous, traffic_real in radar_sequences_to_be_predicted:
 
 
 plt.plot([i for i in range(len(data_predicted))], data_predicted, color='r', label='Predicted' )
-plt.plot([i for i in range(len(data_realized))],data_realized, color='b', label='Realized' )
+plt.plot([i for i in range(len(data_predicted))],data_realized[0:len(data_predicted)], color='b', label='Realized' )
 plt.legend(loc="upper right")
+plt.xlabel( "No. of quater hours" )
+plt.ylabel( "Number of cars" )
+plt.title("Predictions Vs Reality on {} days".format(str(nb_days_predicted)))
 plt.show()
 
 print("TOTAL took %s seconds" % (time.time() - start_time))
